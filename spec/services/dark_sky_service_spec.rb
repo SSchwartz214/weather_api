7131c1f803c471666bc3993bdd97966595c8b38a
JP Lynch <JPLynch35@gmail.com>
2018-10-30 11:54:57 -0600

    Add test for dark sky service

--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
-# describe DarkSkyService do
+describe DarkSkyService do
+  it 'exists' do
+    service = DarkSkyService.new('42.3601', '-71.0589', '2019-04-02T01:30:00Z')
 
-#   describe "weather" do
-#     it "finds the weather for a location" do
-#       latitude = '42.3601'
-#       longitude = '-71.0589'
-#       time = '2019-04-02T01:30:00Z'
-  
-#       get "https://api.darksky.net/forecast/d53b791631b6d4afaf77b93fbed44e45/#{latitude},#{longitude},#{time}"
-#     end
-#   end 
-# end
+    expect(service).to be_a(DarkSkyService)
+  end
+
+  describe "instance methods" do
+    it "finds the weather for a location" do
+      latitude = '42.3601'
+      longitude = '-71.0589'
+      time = '2019-04-02T01:30:00Z'
+
+      service = DarkSkyService.new(latitude, longitude, time)
+      raw_weather= service.get_weather
+
+      expect(raw_weather).to be_a(Hash)
+      expect(raw_weather).to have_key(:summary)
+      expect(raw_weather[:summary]).to be_a(String)
+      expect(raw_weather).to have_key(:precipProbability)
+      expect(raw_weather[:precipProbability]).to be_a(Float)
+      expect(raw_weather).to have_key(:temperatureHigh)
+      expect(raw_weather[:temperatureHigh]).to be_a(Float)
+      expect(raw_weather).to have_key(:temperatureLow)
+      expect(raw_weather[:temperatureLow]).to be_a(Float)
+    end
+  end 
+end
